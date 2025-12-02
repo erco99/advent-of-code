a8e76c7c595aad4aac68d5f87dd6223fb72c0ff0
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2025-12-02 19:50:09 +0100

    Add day 2 solution

---
+++ day2/solution.py
@@ -0,0 +1,30 @@
+import sys
+
+def check_if_valid(id):
+    s = str(id)
+
+    if len(s) % 2 != 0:
+        return True
+    
+    half = len(s) // 2
+    left, right = s[:half], s[half:]
+
+    return left != right
+
+def solve(ranges):
+    result = 0
+    for val in ranges.split(","):
+        first_id, last_id = map(int, val.split("-"))
+        for val_id in range(first_id, last_id + 1):
+            result += val_id if not check_if_valid(val_id) else 0
+            
+    return result
+    
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    with open(filename, "r") as f:
+        content = f.read()
+
+
+    print(solve(content))
